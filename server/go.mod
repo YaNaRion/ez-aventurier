1b2be270fb0641090becd9cccd318d287d2a55d4
YaNaRion <druet.goldorak@gmail.com>
2026-01-23 14:11:07 -0500

    mongo connection working

--- server/go.mod
+++ server/go.mod
@@ -14,7 +14,7 @@ require (
 require (
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
-	github.com/golang/snappy v0.0.4 // indirect
+	github.com/golang/snappy v1.0.0 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/pprof v0.0.0-20241210010833-40e02aabc2ad // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
@@ -24,11 +24,12 @@ require (
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
-	golang.org/x/crypto v0.31.0 // indirect
+	go.mongodb.org/mongo-driver/v2 v2.4.2 // indirect
+	golang.org/x/crypto v0.33.0 // indirect
 	golang.org/x/net v0.33.0 // indirect
-	golang.org/x/sync v0.10.0 // indirect
-	golang.org/x/sys v0.29.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 	golang.org/x/tools v0.28.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- server/infra/setup.go
+++ server/infra/setup.go
@@ -4,9 +4,7 @@ import (
 	"context"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"log"
-	"time"
 )
 
 const urlConnection = "mongodb+srv://YaNaRion:kolia1@dev.ddwky9s.mongodb.net/?appName=dev"
@@ -17,26 +15,24 @@ type DB struct {
 }
 
 func Setup() (*DB, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	opts := options.Client().
-		ApplyURI("mongodb+srv://YaNaRion:<db_password>@dev.ddwky9s.mongodb.net/?appName=dev").
-		SetServerAPIOptions(serverAPI)
-	// Create a new client and connect to the server
-	client, err := mongo.Connect(opts)
+	ctx := context.Background()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(urlConnection))
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	defer func() {
-		if err = client.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-	}()
-	// Send a ping to confirm a successful connection
-	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
-		panic(err)
+
+	// Verify connection
+	if err := client.Ping(ctx, nil); err != nil {
+		return nil, err
 	}
-	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
-	return nil, nil
+
+	db := DB{
+		Client: *client,
+		Ctx:    ctx,
+	}
+
+	return &db, nil
 }
 
 func (db *DB) HelloWorld() {
