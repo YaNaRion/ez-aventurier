65aee627ccf30284f651714e4c340440a886dea3
YaNaRion <druet.goldorak@gmail.com>
2026-02-25 22:56:14 -0500

    ajout de verification de moins de une semaine sur le temps pour rentrer la cache

--- server/infra/cache.go
+++ server/infra/cache.go
@@ -222,9 +222,15 @@ func (db *DB) ClaimCaches(user_id, answer_id string) (*models.Cache, error) {
 	}
 
 	var cache models.Cache
+	oneWeekAgo := time.Now().UTC().AddDate(0, 0, -7)
 	err := collection.FindOneAndUpdate(
 		ctx,
-		bson.M{"answer": answer_id},
+		bson.M{
+			"answer": answer_id,
+			"releaseTime": bson.M{
+				"$gte": oneWeekAgo, // releaseTime should be greater than or equal to one week ago
+			},
+		},
 		bson.M{
 			"$inc": bson.M{"answer_count": 1},
 			"$push": bson.M{"claimed_by": models.Claim{
